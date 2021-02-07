3755ea5e5726d267efd067712e67b2528bf8d56c
Jingjie <349814342@qq.com>
2021-02-07 15:51:59 +0800

    test

---
+++ Lean_Python/恶趣味.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter.messagebox import *
+import random
+import threading
+import time
+import sys
+
+
+aaa = '电脑崩溃中...'
+def boom():
+
+    window = tk.Tk()
+    width = window.winfo_screenwidth()
+    heigh = window.winfo_screenheight()
+    w = random.randrange(0, width)
+    h = random.randrange(0, heigh)
+    window.title('严君泽真香警告！')
+    window.geometry('200x100+'+str(w)+'+'+str(h))
+    tk.Label(window, text=aaa, fg='red', bg='white', font=('宋体', 16),
+             width=20, heigh=4).pack()
+    window.mainloop()
+
+def mess():
+    ask = askyesno('警告！', '是男人就点是！')
+    global aaa
+    if ask == True:
+        showinfo('牛批！', '继续！')
+        aaa = '电脑放假中...'
+
+
+    elif ask == False:
+        showinfo('TMD', '你这也算男人？！')
+        aaa = '电脑觉得你是个娘炮！'
+
+    # showwarning('警告！', '是否关闭')
+
+def thread(tar):
+    threads = []
+    for i in range(random.randint(5, 10)):
+        t = threading.Thread(target=tar)
+        threads.append(t)
+        time.sleep(0.5)
+        threads[i].start()
+
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            thread(boom)
+            mess()
+    except KeyboardInterrupt:
+        sys.exit()
+
+
